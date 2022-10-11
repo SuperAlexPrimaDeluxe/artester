fab0168a709b2f0782650cf21622aa76b55f1051
Alexandre Chevalier <alextechnocite@gmail.com>
2022-10-11 11:31:20 +0200

    lol

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, Box } from '@react-three/drei'
+import { VRButton, ARButton, XR, Controllers, Hands, useHitTest } from '@react-three/xr'
+
+function LoadGLB() {
+  const gltf = useGLTF('./assets/character/bestdubest.glb', true)
+  return <primitive object={gltf.scene} />
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <ARButton sessionInit={{ optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking', 'layers'] }}/>
+    <Canvas>
+      <XR>
+        <Controllers />
+        <Hands />
+        <mesh position={[0, 0, -10]} scale={1}>
+          <LoadGLB />
+          <meshBasicMaterial color="blue" />
+        </mesh>
+        <mesh>
+          <meshBasicMaterial color="blue" />
+        </mesh>
+      </XR>
+    </Canvas>
+  </>
+  )
 }
 
 export default App;
